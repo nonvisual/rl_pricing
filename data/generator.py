1529e4a58ddb4eca5121311a9a22e71be32d08e0
nonvisual <nonvisual.wrk@gmail.com>
2023-03-18 18:52:25 +0100

    -added gitignore -added notebook with visualisation -added kpis computation in pricing game

--- data/generator.py
+++ data/generator.py
@@ -11,16 +11,15 @@ class DemandGenerator:
     def compute_sale(self, discounts):
         raise NotImplementedError()
 
-    def generate(self):
+    def set_products(self, num_products, article_season_start, article_season_end):
         raise NotImplementedError()
-    
-    def play_prices(self, discounts)
 
-class SimpleDemandGenerator:
+
+class SimpleDemandGenerator(DemandGenerator):
     def __init__(
         self,
         max_unexplained_noise_sd=0.3,
-        mu_elasticity=-2.0,
+        mu_elasticity=-3.0,
         sd_elasticity=1.5,
         base_demand_mean=30,
         base_demand_sd=15,
@@ -34,8 +33,9 @@ class SimpleDemandGenerator:
         self.base_demand_mean = base_demand_mean
         self.base_demand_sd = base_demand_sd
 
-    def set_products(self, num_products, article_season_start, article_season_end):
-        
+    def set_products(self, num_products, article_season_start, article_season_end, seed):
+        np.random.seed(seed)
+
         self.article_season_start = article_season_start
         self.article_season_end = article_season_end
 
@@ -44,41 +44,16 @@ class SimpleDemandGenerator:
         self.elasticities = np.random.normal(self.mu_elasticity, self.sd_elasticity, num_products)
         self.seasonality_factors = np.random.uniform(0.0, 0.9, num_products)
 
-        
-
-
-    def compute_sale(self, discounts):
+    def compute_sale(self, discounts, online_status, current_cw, stocks):
         basic_demand = (
-            self.base_demands
-            * (1 - discounts / 100.0) ** self.elasticities
-            * self.online_status[self.current_cw - 1].astype(int)
+            self.base_demands * (1 - discounts / 100.0) ** self.elasticities * online_status[current_cw - 1].astype(int)
         )  # 0 if not online
         demand_with_noise = basic_demand + basic_demand * np.random.normal(loc=0, scale=self.unexplained_noise_sd)
 
         demand_seasonality = (
-            self.seasonality_factors * demand_with_noise * np.where(self.article_season == self.current_season, 1, 0)
+            self.seasonality_factors
+            * demand_with_noise
+            * np.where((self.article_season_start <= current_cw) & (self.article_season_end >= current_cw), 1, 0)
         )
 
-        return np.clip(demand_with_noise + demand_seasonality, 0, self.stocks[self.current_cw - 1]).astype(int)
-
-
-    # discounts - array of chosen discounts, returns sales, revenue, profit
-    def play_prices(self, discounts) -> Tuple[np.ndarray, np.ndarray]:
-        # 1 update week and online statuses
-        self.current_cw += 1
-        self._update_online_status()
-
-        # 2 compute sales
-        sales = self.compute_sale(discounts)
-        stocks = self.stocks[self.current_cw - 1] - sales
-        self.discounts.append(discounts)
-        self.sales.append(sales)
-        self.stocks.append(stocks)
-
-        return sales, stocks, self.online_status[self.current_cw - 1]
-
-    def get_open_information(self):
-        return
-
-    def compute_financial_kpis(self):
-        return
+        return np.clip(demand_with_noise + demand_seasonality, 0, stocks[current_cw - 1]).astype(int)
--- game/simulator.py
+++ game/simulator.py
@@ -8,30 +8,48 @@ YEAR_WEEKS = 52
 
 class PricingGame:
     def __init__(
-        self, demand_generator, num_products, market_seasons=default_market_seasons, seed=12345, num_weeks=YEAR_WEEKS
+        self,
+        demand_generator,
+        num_products,
+        seed=12345,
+        num_weeks=YEAR_WEEKS,
+        min_price=10.0,
+        max_price=200.0,
+        min_cogs=0.4,
+        max_cogs=0.9,
+        max_initial_stock=1000,
+        profit_lack_penalty=10.0,
+        target_profit_ratio=0.05,
     ):
         self.demand_generator = demand_generator
         self.num_products = num_products
         self.num_weeks = num_weeks
 
+        self.min_price = min_price
+        self.max_price = max_price
+        self.max_cogs = max_cogs
+        self.min_cogs = min_cogs
+        self.max_initial_stock = max_initial_stock
+        self.profit_lack_penalty = profit_lack_penalty
+        self.target_profit_ratio = target_profit_ratio
         self.reset_game(seed)
 
     def reset_game(self, seed):
         # set new seed
-        self.demand_generator.set_seed(seed)
         self.seed = seed
         np.random.seed(seed)
 
         # re-initialize article information
-        self.black_prices = np.random.uniform(10.0, 200.0, self.num_products)
-        self.cogs = np.random.uniform(0.1, 0.8, self.num_products) * self.black_prices
-        self.residual_value = np.random.uniform(0.2, 0.9, self.num_products) * self.black_prices
+        self.black_prices = np.random.uniform(self.min_price, self.max_price, self.num_products)
+        self.cogs = np.random.uniform(self.min_cogs, self.max_cogs, self.num_products) * self.black_prices
+        self.residual_value = np.random.uniform(0.2, 1.0, self.num_products) * self.black_prices
         self.article_season_start = np.random.choice(range(YEAR_WEEKS - 10), size=self.num_products)
         self.article_season_end = np.clip(
             self.article_season_start + np.random.choice(range(30), size=self.num_products), 0, YEAR_WEEKS
         )
         self.product_names = [f"product{i+1}" for i in range(self.num_products)]
-        self.initial_stocks = np.random.uniform(10.0, 3000.0, self.num_products).astype(int)
+        self.initial_stocks = np.random.uniform(0, self.max_initial_stock, self.num_products).astype(int)
+        self.shipment_costs = self.min_price / 2
 
         # reset game history
         self.current_cw = 0
@@ -40,6 +58,11 @@ class PricingGame:
         self.sales = []
         self.online_status = []
         self._update_online_status()
+        self.revenues = []
+        self.profits = []
+        self.sdrs = []
+
+        self.demand_generator.set_products(self.num_products, self.article_season_start, self.article_season_end, seed)
 
     # discounts - array of chosen discounts, returns sales, revenue, profit
     def play_prices(self, discounts):
@@ -48,22 +71,22 @@ class PricingGame:
         self._update_online_status()
 
         # 2 compute sales
-        sales = self.generator.compute_sale(discounts)
+        sales = self.demand_generator.compute_sale(discounts, self.online_status, self.current_cw, self.stocks)
         stocks = self.stocks[self.current_cw - 1] - sales
         self.discounts.append(discounts)
         self.sales.append(sales)
         self.stocks.append(stocks)
 
+        revenue = self.black_prices * (1 - discounts / 100.0) * sales
+        profit = (self.black_prices * (1 - discounts / 100.0) - self.cogs - self.shipment_costs) * sales
+        sdr = (self.black_prices * (discounts / 100.0) * sales).sum() / (self.black_prices * sales).sum()
+        self.revenues.append(revenue)
+        self.profits.append(profit)
+        self.sdrs.append(sdr)
         return sales, stocks, self.online_status[self.current_cw - 1]
 
     def _update_online_status(self):
-        new_status = np.array(
-            [
-                (self.current_cw >= self.article_season_start[i])
-                or (self.online_status[self.current_cw - 1][i] and (self.current_cw > 1))
-                for i in range(self.num_products)
-            ]
-        )
+        new_status = np.array([(self.current_cw >= self.article_season_start[i]) for i in range(self.num_products)])
         self.online_status.append(new_status)
 
     def get_state(self):
@@ -71,3 +94,22 @@ class PricingGame:
 
     def get_history(self):
         pass
+
+    def get_kpis(self):
+        return self.sales, self.stocks, self.revenues, self.profits, self.sdrs
+
+    def get_final_score(self):
+        revenue = sum([r.sum() for r in self.revenues])
+        profit = sum([r.sum() for r in self.profits])
+
+        residual_revenue = (self.stocks[-1] * self.black_prices).sum()
+        residual_profit = (self.stocks[-1] * (self.black_prices - self.cogs)).sum()
+
+        total_revenue = revenue + residual_revenue
+        total_profit = profit + residual_profit
+
+        penalty = max(0, total_revenue * self.target_profit_ratio - total_profit) * self.profit_lack_penalty
+
+        score = total_revenue - penalty
+
+        return score, total_revenue, total_profit, penalty, revenue, profit, residual_revenue, residual_profit
